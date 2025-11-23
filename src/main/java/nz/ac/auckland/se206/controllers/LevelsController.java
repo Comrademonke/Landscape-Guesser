b4ca7267dbe67e58d2f64df90b847a989e140e89
Vincent <vung610@aucklanduni.ac.nz>
2025-11-23 15:41:11 +1300

    added in levels buttons to switch scenes

--- src/main/java/nz/ac/auckland/se206/controllers/LevelsController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LevelsController.java
@@ -1,28 +1,18 @@
 package nz.ac.auckland.se206.controllers;
 
-import java.io.File;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class LevelsController {
-  // <Button fx:id="switchToCounterButton" layoutX="458.0" layoutY="31.0" mnemonicParsing="false"
-  // onAction="#switchToCounter" text="Switch to Counter" />
-  //   <Button fx:id="playFirstSongButton" layoutX="93.0" layoutY="306.0" mnemonicParsing="false"
-  // onAction="#playFirstSong" text="First Song" />
-  //   <Button fx:id="playSecondSongButton" layoutX="421.0" layoutY="306.0" mnemonicParsing="false"
-  // onAction="#playSecondSong" text="Second Song" />
 
   @FXML private Button switchToCounterButton;
-  @FXML private Button playFirstSongButton;
-  @FXML private Button playSecondSongButton;
-
-  private MediaPlayer mediaPlayer;
+  @FXML private Button levelOneButton;
+  @FXML private Button levelTwoButton;
+  @FXML private Button levelThreeButton;
 
   @FXML
   private void initialize() {
@@ -36,38 +26,33 @@ public class LevelsController {
     Button button = (Button) event.getSource();
     Scene sceneButtonIsIn = button.getScene();
     sceneButtonIsIn.setRoot(SceneManager.getUiRoot(AppUi.LOADING_PAGE));
-    if (mediaPlayer != null) {
-      mediaPlayer.stop();
-    }
   }
 
   @FXML
-  private void playFirstSong() {
-    if (mediaPlayer != null) {
-      mediaPlayer.stop();
-    }
-    System.out.println("Playing first song");
-    String audioFilePath = "src/main/resources/sounds/looking-forward-131923.mp3";
-    File audioFile = new File(audioFilePath);
+  private void onSwitchToLevelOne(ActionEvent event) {
+    levelOneButton.setDisable(true);
 
-    Media media = new Media(audioFile.toURI().toString());
-    mediaPlayer = new MediaPlayer(media);
-
-    mediaPlayer.play();
+    switchToLevel("level1", 1, event);
   }
 
   @FXML
-  private void playSecondSong() {
-    if (mediaPlayer != null) {
-      mediaPlayer.stop();
-    }
-    System.out.println("Playing second song");
-    String audioFilePath = "src/main/resources/sounds/risk-136788.mp3";
-    File audioFile = new File(audioFilePath);
+  private void onSwitchToLevelTwo(ActionEvent event) {
+    levelTwoButton.setDisable(true);
 
-    Media media = new Media(audioFile.toURI().toString());
-    mediaPlayer = new MediaPlayer(media);
+    switchToLevel("level2", 2, event);
+  }
 
-    mediaPlayer.play();
+  @FXML
+  private void onSwitchToLevelThree(ActionEvent event) {
+    levelThreeButton.setDisable(true);
+
+    switchToLevel("level3", 3, event);
+  }
+
+  public void switchToLevel(String level, int numberLevel, ActionEvent event) {
+
+    Button button = (Button) event.getSource();
+    Scene sceneButtonIsIn = button.getScene();
+    sceneButtonIsIn.setRoot(SceneManager.getUiRoot(AppUi.LOADING_PAGE));
   }
 }
