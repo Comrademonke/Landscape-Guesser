5270ba45630b92f56ba97052ce72c2f5442ce8e1
Vincent <vung610@aucklanduni.ac.nz>
2025-11-23 17:06:26 +1300

    added in music to play in the background

--- src/main/java/nz/ac/auckland/se206/controllers/LoadingPageController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LoadingPageController.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import nz.ac.auckland.se206.SceneManager;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
@@ -18,10 +20,26 @@ public class LoadingPageController {
   @FXML private Button switchButton;
   @FXML private Label numberLabel;
 
+  private MediaPlayer mediaPlayer;
+
   @FXML
   private void initialize() {
     System.out.println(
         "************* Initialising Counter Controller ************************" + this);
+
+    playMusic();
+  }
+
+  private void playMusic() {
+    String audioFile = getClass().getResource("/sounds/catWalkSong.mp3").toString();
+
+    Media media = new Media(audioFile);
+    mediaPlayer = new MediaPlayer(media);
+
+    mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+    mediaPlayer.setVolume(0.3);
+
+    mediaPlayer.play();
   }
 
   @FXML
